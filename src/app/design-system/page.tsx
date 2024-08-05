badf8d03868e0af63282b4e4c529abafa480593a
lukas-laas <lukas.s.laas@gmail.com>
2024-08-05 09:58:05 +0200

    simple skeleton for components

---
+++ src/app/design-system/page.tsx
@@ -0,0 +1,27 @@
+export default function DesignSystem() {
+  return (
+    <>
+      <div>
+        <button>Buttom</button>
+        <button>Buttom</button>
+        <button>Buttom</button>
+        <button>Buttom</button>
+      </div>
+      <div>
+        <p>Fonts</p>
+      </div>
+      <div>
+        <input>Input</input>
+        <input>Input</input>
+        <input>Input</input>
+        <input>Input</input>
+      </div>
+      <div>
+        <div>Layout stuff, card etc</div>
+      </div>
+      <div>
+        <div>etc</div>
+      </div>
+    </>
+  );
+}
